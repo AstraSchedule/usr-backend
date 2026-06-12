b02d86e544816e34520da44530e064e47495dfcd
daizihan233 <y066ydg5@duck.com>
2026-06-13 00:04:21 +0800

    🐛 fix(client): 气温颜色区间为空时填充默认值

--- router/client/getSchedule.go
+++ router/client/getSchedule.go
@@ -3,6 +3,7 @@ package client
 import (
 	"AstraScheduleServerGo/db"
 	"AstraScheduleServerGo/model"
+	"AstraScheduleServerGo/model/dbTable"
 	"AstraScheduleServerGo/service"
 	"net/http"
 	"strconv"
@@ -35,6 +36,19 @@ func GetSchedule(c *gin.Context) {
 	}
 	_, _ = db.RefreshAutorunStatuses(time.Now())
 	clientConfig := db.GetClientConfig(school, grade, class)
+	
+	// 如果数据库中没有 temperature_colors 配置或 stops 为空，使用默认值
+	if len(clientConfig.TemperatureColors.Stops) == 0 {
+		clientConfig.TemperatureColors = dbTable.TemperatureColorsConfig{
+			UseGradient: clientConfig.TemperatureColors.UseGradient,
+			Stops: []dbTable.TemperatureStop{
+				{Temp: 20, Color: "#66CCFF"},
+				{Temp: 30, Color: "#5FBC21"},
+				{Temp: 36, Color: "#FF8C00"},
+				{Temp: 100, Color: "#EE0000"},
+			},
+		}
+	}
 	schedule := db.GetSchedule(school, grade, class)
 	subject := db.GetSubject(school, grade)
 	timetable := db.GetTimetable(school, grade)
