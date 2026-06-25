d116598301973baa202bd014dbbc2dee7e897dc7
daizihan233 <y066ydg5@duck.com>
2026-06-25 17:18:15 +0800

    ci: build 配置修改

--- build_scf_bootstrap.ps1
+++ build_scf_bootstrap.ps1
@@ -1,18 +1,18 @@
 $Env:GOOS="linux"
 $Env:GOARCH="amd64"
 go mod tidy
-if (test-path .\scf_bootstrap)
+if (test-path .\bootstrap)
 {
-    Remove-Item .\scf_bootstrap -Force
+    Remove-Item .\bootstrap -Force
 }
-go build -o scf_bootstrap
-wsl chmod +x scf_bootstrap
+go build -o bootstrap
+wsl chmod +x bootstrap
 if (test-path .\scf.zip)
 {
     Remove-Item .\scf.zip -Force
 }
-wsl 7z a -tzip "scf.zip" "scf_bootstrap" "config.toml" -mx0
-if (test-path .\scf_bootstrap)
+wsl 7z a -tzip "scf.zip" "bootstrap" -mx0
+if (test-path .\bootstrap)
 {
-    Remove-Item .\scf_bootstrap -Force
+    Remove-Item .\bootstrap -Force
 }
